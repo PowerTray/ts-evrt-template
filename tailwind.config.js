51896062ca2db40aea6e9b8bbfb062d8143fbc0e
Alask-Code <anonimoplayer736@gmail.com>
2025-02-13 19:01:05 -0300

    add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import withMT from '@material-tailwind/react/utils/withMT';
+
+export default withMT({
+  content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: { extend: {} },
+  variants: {
+    extend: {},
+    fontFamily: { sans: ['Inter', 'ui-sans-serif', 'system-ui'] }
+  },
+  plugins: []
+});
