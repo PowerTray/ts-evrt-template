1d618bc33a83a1345c16346b03d1780336a26ae1
Alask-Code <anonimoplayer736@gmail.com>
2025-02-13 19:01:16 -0300

    add files via upload

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -13,7 +13,7 @@ function App() {
           rel="noopener noreferrer"
           className="hover:text-blue-800 transition-colors duration-300 ease-in-out text-2xl font-bold"
         >
-          Unleash the Power of Vite&apos;s HMR
+          Unleash the Power of Vite&apos;s HMR,
         </a>
         <a
           href="https://react.dev"
@@ -21,7 +21,7 @@ function App() {
           rel="noopener noreferrer"
           className="hover:text-blue-800 transition-colors duration-300 ease-in-out text-2xl font-bold"
         >
-          React, the library you know and love
+          React, the library you know and love,
         </a>
         <a
           href="https://www.electronjs.org"
@@ -29,7 +29,7 @@ function App() {
           rel="noopener noreferrer"
           className="hover:text-blue-800 transition-colors duration-300 ease-in-out text-2xl font-bold"
         >
-          With the Power of Electron, desktop apps made easy
+          With the Power of Electron, desktop apps made easy,
         </a>
         <a
           href="https://material-tailwind.com"
@@ -37,7 +37,7 @@ function App() {
           rel="noopener noreferrer"
           className="hover:text-blue-800 transition-colors duration-300 ease-in-out text-2xl font-bold"
         >
-          Beautiful UI, with TailwindCSS and Material Tailwind
+          Beautiful UI, with TailwindCSS and Material Tailwind,
         </a>
         <a
           href="https://www.typescriptlang.org/docs/"
@@ -45,10 +45,10 @@ function App() {
           rel="noopener noreferrer"
           className="hover:text-blue-800 transition-colors duration-300 ease-in-out text-2xl font-bold"
         >
-          And all of this, made possible with TypeScript
+          And all of this, made possible with TypeScript.
         </a>
       </div>
-      <p>
+      <p className="text-xl py-10">
         Edit <code className="bg-gray-800 text-gray-100 p-1 px-2 rounded-md">src/pages/App.tsx</code> to See Changes.
       </p>
     </SafeView>
